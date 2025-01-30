0c41968e23d4eb248d657155b3f8efbef188aa0f
Mi-GV <michelle.g.vianna@gmail.com>
2025-01-30 08:01:55 -0300

    Controlando criacao de threads no Spring

---
+++ concurrency/adopet-store/src/main/java/br/com/alura/adopetstore/configuration/AsyncConfiguration.java
@@ -0,0 +1,23 @@
+package br.com.alura.adopetstore.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+public class AsyncConfiguration {
+
+    @Bean
+    public Executor disparadorEmailThread() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(1);
+        executor.setMaxPoolSize(3);
+        executor.setQueueCapacity(10);
+        executor.setThreadNamePrefix("DisparadorEmail-");
+
+        return executor;
+    }
+
+}
