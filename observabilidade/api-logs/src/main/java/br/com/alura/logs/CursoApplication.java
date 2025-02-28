9581b94174c9d9aaf3ba39d183afc01742328f19
Mi-GV <michelle.g.vianna@gmail.com>
2025-02-28 08:54:57 -0300

    Add appender com arquivo e ajuste mensagem logback

--- observabilidade/api-logs/src/main/java/br/com/alura/logs/CursoApplication.java
+++ observabilidade/api-logs/src/main/java/br/com/alura/logs/CursoApplication.java
@@ -1,12 +1,18 @@
 package br.com.alura.logs;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CursoApplication {
 
+	private static Logger logger = LoggerFactory.getLogger(CursoApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("Iniciando aplicação");
 		SpringApplication.run(CursoApplication.class, args);
+		logger.info("Aplicação iniciada");
 	}
 }
