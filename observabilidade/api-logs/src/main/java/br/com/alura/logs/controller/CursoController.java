2f3ed852541c75eddb353809cb47e486eadce47b
Mi-GV <michelle.g.vianna@gmail.com>
2025-03-18 08:31:52 -0300

    Ajuste nivel de logs

--- observabilidade/api-logs/src/main/java/br/com/alura/logs/controller/CursoController.java
+++ observabilidade/api-logs/src/main/java/br/com/alura/logs/controller/CursoController.java
@@ -39,26 +39,25 @@ public class CursoController {
 	@PostMapping
 	 public ResponseEntity<Object> saveCurso(@RequestBody @Valid CursoDto cursoDto){
         try {
-            logger.info("Iniciando processo de insercao de registro de novo curso");
-
-            logger.info("Chamando o cursoService para validar se numero de matricula ja existe");
+            logger.debug("Iniciando processo de insercao de registro de novo curso");
+            logger.debug("Chamando o cursoService para validar se numero de matricula ja existe");
 
             if(cursoService.existsByNumeroMatricula(cursoDto.getNumeroMatricula())) {
                 logger.warn("Novo registro nao inserido, o numero de matricula ja existe");
-                return ResponseEntity.status(HttpStatus.CONFLICT).body("O número de matricula do curso já esta em uso!");
+                return ResponseEntity.status(HttpStatus.CONFLICT).body("O número de matrícula do curso já esta em uso!");
             }
 
-            logger.info("Validacao de numero de matricula concluida");
-            logger.info("Chamando o cursoService para validar se numero de curso ja existe");
+            logger.debug("Validacao de numero de matricula concluida");
+            logger.debug("Chamando o cursoService para validar se numero de curso ja existe");
 
             if(cursoService.existsByNumeroCurso(cursoDto.getNumeroCurso())) {
                 logger.warn("Novo registro nao inserido, o numero do curso ja existe");
-                return ResponseEntity.status(HttpStatus.CONFLICT).body("O número do curso já esta em uso!");
+                return ResponseEntity.status(HttpStatus.CONFLICT).body("O número do curso já está em uso!");
             }
 
-            logger.info("Validacao de numero de curso concluida");
-            logger.info("Validacoes de cursoService sobre cursoDTO executadas com sucesso");
-            logger.info("Chamando cursoService.save para armazenar o novo registro");
+            logger.debug("Validacao de numero de curso concluida");
+            logger.debug("Validacoes de cursoService sobre cursoDTO executadas com sucesso");
+            logger.debug("Chamando cursoService.save para armazenar o novo registro");
 
             var cursoModel = new CursoModel();
             BeanUtils.copyProperties(cursoDto, cursoModel);
@@ -69,10 +68,9 @@ public class CursoController {
             return ResponseEntity.status(HttpStatus.CREATED).body(cursoService.save(cursoModel));
         } catch (CannotCreateTransactionException | DataAccessException e) {
             logger.error("Erro de comunicacao com banco de dados", e);
-            throw new InternalErrorException("Erro momentaneo. Por favor, tente novamente mais tarde");
+            throw new InternalErrorException("Erro momentâneo. Por favor, tente novamente mais tarde");
         }
 	}
-	
 	
 	@GetMapping
 	public ResponseEntity<Page<CursoModel>> getAllCursos(@PageableDefault(page = 0, size = 10, sort = "dataInscricao", direction = Sort.Direction.ASC) Pageable pageable) {
@@ -81,25 +79,25 @@ public class CursoController {
             return ResponseEntity.status(HttpStatus.OK).body(cursoService.findAll(pageable));
         } catch (CannotCreateTransactionException | DataAccessException e) {
             logger.error("Erro de comunicacao com banco de dados", e);
-            throw new InternalErrorException("Erro momentaneo. Por favor, tente novamente mais tarde");
+            throw new InternalErrorException("Erro momentâneo. Por favor, tente novamente mais tarde");
         }
 	}
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<Object> getOneCursos(@PathVariable(value="id") UUID id) {
         try {
-            logger.info("Chamando cursoService.findById para buscar um registro por UUID");
+            logger.debug("Chamando cursoService.findById para buscar um registro por UUID");
             Optional<CursoModel> cursoModelOptional = cursoService.findById(id);
-            logger.info("Validando por cursoModelOptional se o UUID existe");
+            logger.debug("Validando por cursoModelOptional se o UUID existe");
             if (cursoModelOptional.isEmpty()) {
                 logger.warn("Validacao em cursoModelOptional retornou vazio, UUID nao encontrado!");
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Curso não encontrado!");
             }
-            logger.info("O registro foi encontrado por cursoService no banco de dados!");
+            logger.debug("O registro foi encontrado por cursoService no banco de dados!");
             return ResponseEntity.status(HttpStatus.OK).body(cursoModelOptional.get());
         } catch (CannotCreateTransactionException | DataAccessException e) {
             logger.error("Erro de comunicacao com banco de dados", e);
-            throw new InternalErrorException("Erro momentaneo. Por favor, tente novamente mais tarde");
+            throw new InternalErrorException("Erro momentâneo. Por favor, tente novamente mais tarde");
         }
 
 }
@@ -109,7 +107,7 @@ public class CursoController {
         try {
             logger.info("Chamando cursoService.findById para excluir um registro por UUID");
             Optional<CursoModel> cursoModelOptional = cursoService.findById(id);
-            logger.info("Validando por cursoModelOptional se o UUID existe");
+            logger.debug("Validando por cursoModelOptional se o UUID existe");
             if (!cursoModelOptional.isPresent()) {
                 logger.warn("Tentativa de exclusao abortada! UUID informado nao encontrado");
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Curso não encontrado!");
@@ -119,7 +117,7 @@ public class CursoController {
             return ResponseEntity.status(HttpStatus.OK).body("Curso excluído com sucesso!");
         } catch (CannotCreateTransactionException | DataAccessException e) {
             logger.error("Erro de comunicacao com banco de dados", e);
-            throw new InternalErrorException("Erro momentaneo. Por favor, tente novamente mais tarde");
+            throw new InternalErrorException("Erro momentâneo. Por favor, tente novamente mais tarde");
         }
 
     }
@@ -129,7 +127,7 @@ public class CursoController {
         try {
             logger.info("Chamando cursoService.findById para alterar um registro por UUID");
             Optional<CursoModel> cursoModelOptional = cursoService.findById(id);
-            logger.info("Validando por cursoModelOptional se o UUID existe");
+            logger.debug("Validando por cursoModelOptional se o UUID existe");
             if (!cursoModelOptional.isPresent()) {
                 logger.warn("Validacao em cursoModelOptional retornou vazio, UUID nao encontrado!");
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Curso não encontrado!");
@@ -144,7 +142,7 @@ public class CursoController {
             return ResponseEntity.status(HttpStatus.OK).body(cursoModelAtualizado);
         } catch (CannotCreateTransactionException | DataAccessException e) {
             logger.error("Erro de comunicacao com banco de dados", e);
-            throw new InternalErrorException("Erro momentaneo. Por favor, tente novamente mais tarde");
+            throw new InternalErrorException("Erro momentâneo. Por favor, tente novamente mais tarde");
         }
 
     }
